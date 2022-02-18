3c88a95384105b8bc2ff42f2879b3fff0cc7a52c
Mahmoud Atef <MahmoudMuhammed7@outlook.com>
2022-02-18 17:36:02 +0200

    Feat: initial types

---
+++ src/Types/interfaces.ts
@@ -0,0 +1,31 @@
+export interface Car {
+	make: string;
+	model: string;
+	bodyType: string;
+	engineCapacity: number;
+	productionYear: number;
+	availableColors: Array<string>;
+}
+
+export interface Store {
+	email: string;
+	phone: string;
+	address: string;
+	website: string;
+	cars: Array<Car>;
+}
+
+export interface User {
+	username: string;
+	password: string;
+	name: string;
+	phone: string;
+	email: string;
+	age: number;
+	address: string;
+}
+
+export interface Customer extends User {
+	salary: number;
+	wishlist: Array<Car>;
+}
