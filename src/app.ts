7740284aa19a478915a1f66811fdec21cbcb13e7
Mahmoud Atef <MahmoudMuhammed7@outlook.com>
2022-03-19 18:38:35 +0200

    feat: support cors

--- src/app.ts
+++ src/app.ts
@@ -1,12 +1,14 @@
 import config from "./config";
 
 import express from "express";
+import cors from "cors";
 
 import router from "./Routes/router";
 
 const PORT = config.PORT;
 
 const app = express();
+app.use(cors);
 app.use(router);
 
 app.listen(PORT, () => console.log(`server is listening on port ${PORT}`));
