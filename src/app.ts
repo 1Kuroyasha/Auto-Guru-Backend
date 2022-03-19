f5b0f1c359dc38bc48a00bc9dd9519429859ea14
Mahmoud Atef <MahmoudMuhammed7@outlook.com>
2022-03-19 20:46:09 +0200

    fix: typo

--- src/app.ts
+++ src/app.ts
@@ -8,7 +8,7 @@ import router from "./Routes/router";
 const PORT = config.PORT;
 
 const app = express();
-app.use(cors);
+app.use(cors());
 app.use(router);
 
 app.listen(PORT, () => console.log(`server is listening on port ${PORT}`));
